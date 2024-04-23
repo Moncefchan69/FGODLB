324257fc96078888d9783815a4672e978ff3fd8c
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-04-23 14:41:24 -0600

    Update main02.py

--- main02.py
+++ main02.py
@@ -66,7 +66,7 @@ def main():
                 instance = user.user(userIds[i], authKeys[i], secretKeys[i])
                 time.sleep(3)
                 logger.info('Logging in!')
-                instance.topLogin()
+                instance.topLogin2()
                 time.sleep(2)
                 instance.topHome()
                 time.sleep(2)
