587dbcae865397441be05d2d58d6c5f5ae83bdf1
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-04-26 13:20:30 -0600

    Update mytime.py

--- mytime.py
+++ mytime.py
@@ -1,27 +1,22 @@
 from datetime import datetime, timedelta, timezone
 
-tz_utc_8 = timezone(timedelta(hours=8))
-
+# Create a timezone object for UTC-6 (Central Standard Time)
+tz_utc_minus_6 = timezone(timedelta(hours=-6))
 
 def GetNowTimeHour():
-    return datetime.now(tz=tz_utc_8).hour
-
+    return datetime.now(tz=tz_utc_minus_6).hour
 
 def GetNowTime():
-    return datetime.now(tz=tz_utc_8)
-
+    return datetime.now(tz=tz_utc_minus_6)
 
 def GetFormattedNowTime():
-    return datetime.now(tz=tz_utc_8).strftime('%Y-%m-%d %H:%M:%S')
-
+    return datetime.now(tz=tz_utc_minus_6).strftime('%Y-%m-%d %H:%M:%S')
 
 def GetTimeStamp():
-    return (int)(datetime.now(tz=tz_utc_8).timestamp())
-
+    return int(datetime.now(tz=tz_utc_minus_6).timestamp())
 
 def TimeStampToString(timestamp):
-    return datetime.fromtimestamp(timestamp)
-
+    return datetime.fromtimestamp(timestamp, tz=tz_utc_minus_6)
 
 def GetNowTimeFileName():
-    return datetime.now(tz=tz_utc_8).strftime('%Y/%m/%d.log')
+    return datetime.now(tz=tz_utc_minus_6).strftime('%Y/%m/%d.log')
