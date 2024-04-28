fa18ba9dd8f4e6253d44f1717e68884cd244035d
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-04-28 11:09:59 -0600

    Update main.py

--- main.py
+++ main.py
@@ -41,12 +41,7 @@ def check_blue_apple_cron(instance):
 
 
 def get_latest_verCode():
-    endpoint = ""
-
-    if fate_region == "NA":
-        endpoint += "https://raw.githubusercontent.com/xdeadboy666x/FGO-VerCode-extractor/NA/VerCode.json"
-    else:
-        endpoint += "https://raw.githubusercontent.com/xdeadboy666x/FGO-VerCode-extractor/JP/VerCode.json"
+    endpoint = "https://raw.githubusercontent.com/xdeadboy666x/FGO-VerCode-extractor/NA/VerCode.json"
 
     response = requests.get(endpoint).text
     response_data = json.loads(response)
